0a889639825ba0dafd1b374622aa2d10e1e61d33
IanDev <iandev56@gmail.com>
2026-09-16 21:49:18 +0300

    Create maps config

---
+++ src/config/maps.ts
@@ -0,0 +1,21 @@
+export const MAP_SERVER = "http://192.168.0.107:3000";
+
+export const KENYA_TILES_URL =
+  `${MAP_SERVER}/kenya/{z}/{x}/{y}`;
+
+export const KENYA_TILE_JSON_URL =
+  `${MAP_SERVER}/kenya`;
+
+export const KENYA_BOUNDS = {
+  west: 33.8679,
+  south: -4.816276,
+  east: 41.97685,
+  north: 4.629931,
+};
+
+export const KENYA_CENTER = {
+  longitude: 37.922375,
+  latitude: -0.093173,
+};
+
+export const DEFAULT_MAP_ZOOM = 7;
